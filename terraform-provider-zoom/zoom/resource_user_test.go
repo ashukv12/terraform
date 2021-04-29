e044461a834723b96b622e0406d8da3bcb04c42d
ashukv12 <82150072+ashukv12@users.noreply.github.com>
2021-04-29 10:05:14 +0530

    Update resource_user_test.go

--- terraform-provider-zoom/zoom/resource_user_test.go
+++ terraform-provider-zoom/zoom/resource_user_test.go
@@ -6,6 +6,7 @@ import (
     "os"
     "testing"
 )
+
 func TestAccItem_Basic(t *testing.T) {
     os.Setenv("TF_ACC", "1")
     resource.Test(t, resource.TestCase{
@@ -17,7 +18,7 @@ func TestAccItem_Basic(t *testing.T) {
                 Config: testAccCheckItemBasic(),
                 Check: resource.ComposeTestCheckFunc(
                     testAccCheckExampleItemExists("zoom_orders.test_orders"),
-                    resource.TestCheckResourceAttr(
+					resource.TestCheckResourceAttr(
                         "zoom_orders.test_orders", "email", "ashutoshkverma12@gmail.com"),
                     resource.TestCheckResourceAttr(
                         "zoom_orders.test_orders", "type", "1"),
@@ -26,10 +27,12 @@ func TestAccItem_Basic(t *testing.T) {
                     resource.TestCheckResourceAttr(
                         "zoom_orders.test_orders", "first_name", "abc"),
                 ),
+				ExpectNonEmptyPlan: true,
             },
         },
     })
 }
+
 func testAccCheckItemBasic() string {
     return fmt.Sprintf(`
         resource "zoom_orders" "test_orders" {
@@ -40,6 +43,7 @@ func testAccCheckItemBasic() string {
         }
     `)
 }
+
 func testAccCheckItemDestroy(s *terraform.State) error {
     // apiClient := testAccProvider.Meta().(*client.Client)
     for _, rs := range s.RootModule().Resources {
@@ -49,6 +53,68 @@ func testAccCheckItemDestroy(s *terraform.State) error {
     }
     return nil
 }
+
+// func TestAccItem_Update(t *testing.T) {
+// 	os.Setenv("TF_ACC", "1")
+// 	resource.Test(t, resource.TestCase{
+// 		PreCheck:     func() { testAccPreCheck(t) },
+// 		Providers:    testAccProviders,
+// 		//CheckDestroy: testAccCheckItemDestroy,
+// 		Steps: []resource.TestStep{
+// 			{
+// 				Config: testAccCheckItemUpdatePre(),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					testAccCheckExampleItemExists("zoom_orders.test_orders"),
+// 					resource.TestCheckResourceAttr(
+// 						"zoom_orders.test_orders", "email", "ashutoshkverma12@gmail.com"),
+// 					resource.TestCheckResourceAttr(
+// 						"zoom_orders.test_orders", "type", "1"),
+// 					resource.TestCheckResourceAttr(
+// 						"zoom_orders.test_orders", "last_name", "xyz"),
+// 					resource.TestCheckResourceAttr(
+// 						"zoom_orders.test_orders", "first_name", "abc"),
+// 				),
+// 				ExpectNonEmptyPlan: true,
+// 			},
+// 			{
+// 				Config: testAccCheckItemUpdatePost(),
+// 				Check: resource.ComposeTestCheckFunc(
+// 					testAccCheckExampleItemExists("zoom_orders.test_orders"),
+// 					resource.TestCheckResourceAttr(
+// 						"zoom_orders.test_orders", "email", "ashutoshkverma12@gmail.com"),
+// 					resource.TestCheckResourceAttr(
+// 						"zoom_orders.test_orders", "type", "1"),
+// 					resource.TestCheckResourceAttr(
+// 						"zoom_orders.test_orders", "last_name", "xyz123"),
+// 					resource.TestCheckResourceAttr(
+// 						"zoom_orders.test_orders", "first_name", "abc"),
+// 				),
+// 				ExpectNonEmptyPlan: true,
+// 			},
+// 		},
+// 	})
+// }
+
+// func testAccCheckItemUpdatePre() string {
+// 	return fmt.Sprintf(`
+// 	resource "zoom_orders" "test_orders" {
+// 		email = "ashutoshkverma12@gmail.com"
+// 		first_name = "abc"
+// 		last_name = "xyz"
+// 		type = 1
+// 	}`)
+// }
+
+// func testAccCheckItemUpdatePost() string {
+// 	return fmt.Sprintf(`
+// 	resource "zoom_orders" "test_orders" { 
+// 		email = "ashutoshkverma12@gmail.com"
+// 		first_name = "abc"
+// 		last_name = "xyz123"
+// 		type = 1
+// 	}`)
+// }
+
 func testAccCheckExampleItemExists(resource string) resource.TestCheckFunc {
     return func(state *terraform.State) error {
         rs, ok := state.RootModule().Resources[resource]
@@ -62,3 +128,4 @@ func testAccCheckExampleItemExists(resource string) resource.TestCheckFunc {
         // name := rs.Primary.ID
     }
 }
+
