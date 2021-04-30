167598b743379cb77549862d1e01c6412814b3e4
ashukv12 <82150072+ashukv12@users.noreply.github.com>
2021-04-30 10:24:12 +0530

    Update resource_user.go

--- terraform-provider-zoom/zoom/resource_user.go
+++ terraform-provider-zoom/zoom/resource_user.go
@@ -9,6 +9,8 @@ import (
 	"time"
 	// "strconv"
 	"os"
+	//"io/ioutil"
+	"fmt"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
@@ -87,7 +89,7 @@ func resourceOrderCreate(ctx context.Context, d *schema.ResourceData, m interfac
 	req, _ := http.NewRequest("POST", url, strings.NewReader(string(payload)))
 
 	req.Header.Add("content-type", "application/json")
-	req.Header.Add("authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6ImxOR0pCSGp1Uk9PRktDTTY4TGpIMGciLCJleHAiOjE2MTk0NDMxMjgsImlhdCI6MTYxODgzODM0MH0.YG6Qr5Ce12uPRCG396zKl7myb4Co9cVmo8uokjD0NUA")
+	req.Header.Add("authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6ImxOR0pCSGp1Uk9PRktDTTY4TGpIMGciLCJleHAiOjE2MjAyMTgzMTksImlhdCI6MTYxOTYxMzUyMX0.oU-kb6pSAvFU_SJ-KM7gnNeknRh3dM5eLq1TMSp9vuM")
 
 	res, _ := http.DefaultClient.Do(req)
 
@@ -95,7 +97,9 @@ func resourceOrderCreate(ctx context.Context, d *schema.ResourceData, m interfac
 
 	defer res.Body.Close()
 
-	resourceOrderRead(ctx, d, m)
+	//resourceOrderRead(ctx, d, m)
+
+	fmt.Println("User created")
 
 	return diags
 }
@@ -113,26 +117,75 @@ func resourceOrderRead(ctx context.Context, d *schema.ResourceData, m interface{
 	jwt := os.Getenv("bearer")
 	req.Header.Add("authorization", jwt)
 
+	_, err := client.Do(req)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	Email:=      d.Get("email").(string)
+	First_name:= d.Get("first_name").(string)
+	Last_name:=  d.Get("last_name").(string)
+	Type:=       d.Get("type").(int)
+	
+	if err:= d.Set("email", Email); err!= nil{
+		return diag.FromErr(err)
+	}
+	if err:= d.Set("first_name", First_name); err != nil{
+		return diag.FromErr(err)
+	}
+	if err:= d.Set("last_name", Last_name); err != nil{
+		return diag.FromErr(err)
+	}
+	if err:= d.Set("type", Type); err != nil{
+		return diag.FromErr(err)
+	}
+	
+	// json.NewDecoder(r.Body).Decode(&User_i)
+	
+	// defer r.Body.Close()
+
+	d.SetId(d.Id())
+
+	fmt.Println("User read")
+	
+
+	return diags
+
+/*	
+	var diags diag.Diagnostics
+	
+	client := &http.Client{Timeout: 10 * time.Second}
+
+	UserID := d.Id()
+	url := "https://api.zoom.us/v2/users/" + UserID
+
+	req, _ := http.NewRequest("GET", url, nil)
+	jwt := os.Getenv("bearer")
+	req.Header.Add("authorization", jwt)
+
 	r, err := client.Do(req)
 	if err != nil {
 		return diag.FromErr(err)
 	}
 	User_i := Users_res{}
 	json.NewDecoder(r.Body).Decode(&User_i)
-	d.Set("email", User_i.Email)
-	d.Set("first_name", User_i.First_name)
-	d.Set("last_name", User_i.Last_name)
-	d.Set("type", User_i.Type)
+	// d.Set("email", User_i.Email)
+	// d.Set("first_name", User_i.First_name)
+	// d.Set("last_name", User_i.Last_name)
+	// d.Set("type", User_i.Type)
 
 	defer r.Body.Close()
 	
 	// d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
 	
 	return diags
+*/
 }
 
 func resourceOrderUpdate(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
   
+	var diags diag.Diagnostics
+
   orderID := d.Id()
 
   if d.HasChange("type") || d.HasChange("email") || d.HasChange("first_name") || d.HasChange("last_name") {
@@ -164,7 +217,7 @@ func resourceOrderUpdate(ctx context.Context, d *schema.ResourceData, m interfac
 		return diag.FromErr(err)
 	}
 	req.Header.Add("content-type", "application/json")
-	req.Header.Add("authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6ImxOR0pCSGp1Uk9PRktDTTY4TGpIMGciLCJleHAiOjE2MTk0NDMxMjgsImlhdCI6MTYxODgzODM0MH0.YG6Qr5Ce12uPRCG396zKl7myb4Co9cVmo8uokjD0NUA")
+	req.Header.Add("authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6ImxOR0pCSGp1Uk9PRktDTTY4TGpIMGciLCJleHAiOjE2MjAyMTgzMTksImlhdCI6MTYxOTYxMzUyMX0.oU-kb6pSAvFU_SJ-KM7gnNeknRh3dM5eLq1TMSp9vuM")
 
 	res, err := http.DefaultClient.Do(req)
 
@@ -174,11 +227,20 @@ func resourceOrderUpdate(ctx context.Context, d *schema.ResourceData, m interfac
 
 	defer res.Body.Close()
 
+	// body, err := ioutil.ReadAll(res.Body)
+
+	// if res.StatusCode!=204{
+	// 	return diag.Errorf("Status Code - %v\n%v", res.StatusCode, string(body))
+	// }
+
 	d.Set("last_updated", time.Now().Format(time.RFC850))
 
   }
+ //resourceOrderRead(ctx, d, m)
 
-  return resourceOrderRead(ctx, d, m)
+  fmt.Println("User updated")
+	
+  return diags
 }
 
 func resourceOrderDelete(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
@@ -187,7 +249,7 @@ func resourceOrderDelete(ctx context.Context, d *schema.ResourceData, m interfac
 
 	orderID := d.Id()
 
-	url := "https://api.zoom.us/v2/users/" + orderID +"?action=delete" 
+	url := "https://api.zoom.us/v2/users/" + orderID +"?action=disassociate" 
 
 	//var bearer = os.Getenv("bearer")
 
@@ -198,7 +260,7 @@ func resourceOrderDelete(ctx context.Context, d *schema.ResourceData, m interfac
 	}
 
 	// req.Header.Add("content-type", "application/json")
-	req.Header.Add("authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6ImxOR0pCSGp1Uk9PRktDTTY4TGpIMGciLCJleHAiOjE2MTk0NDMxMjgsImlhdCI6MTYxODgzODM0MH0.YG6Qr5Ce12uPRCG396zKl7myb4Co9cVmo8uokjD0NUA")
+	req.Header.Add("authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhdWQiOm51bGwsImlzcyI6ImxOR0pCSGp1Uk9PRktDTTY4TGpIMGciLCJleHAiOjE2MjAyMTgzMTksImlhdCI6MTYxOTYxMzUyMX0.oU-kb6pSAvFU_SJ-KM7gnNeknRh3dM5eLq1TMSp9vuM")
 
 	res, err := http.DefaultClient.Do(req)
 
@@ -207,6 +269,14 @@ func resourceOrderDelete(ctx context.Context, d *schema.ResourceData, m interfac
 	}
 
 	defer res.Body.Close()
+
+	// body, err := ioutil.ReadAll(res.Body)
+
+	// if res.StatusCode!=204{
+	// 	return diag.Errorf("Status Code - %v\n%v", res.StatusCode, string(body))
+	// }
+
+	fmt.Println("Delete function is working")
 
 	return diags
 }
